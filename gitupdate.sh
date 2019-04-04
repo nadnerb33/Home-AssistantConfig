5c98af76bfc5e92c6b5938d47eae85a1efbcaa13
nadnerb33 <nadnerb@gmail.com>
2019-04-04 21:56:52 +0100

    backup some files to NAS

--- gitupdate.sh
+++ gitupdate.sh
@@ -8,4 +8,8 @@ read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
 
+scp -p 22 ps4-wake.credentials.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 ps4-games.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 secrets.yaml  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+
 exit
