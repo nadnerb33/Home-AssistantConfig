33c8b8da72cbdca21bf3c6e97be872f213da2b6f
nadnerb33 <nadnerb@gmail.com>
2019-04-04 21:58:52 +0100

    test

--- gitupdate.sh
+++ gitupdate.sh
@@ -3,7 +3,7 @@
 cd /home/osmc/homeassistant
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
+echo -n "Enter the Description for the Change: "
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
