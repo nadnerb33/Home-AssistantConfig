8701b0cd35f070551be1c2271efb030c9a756b27
nadnerb33 <nadnerb@gmail.com>
2019-12-30 14:44:20 +0000

    test

---
+++ gitupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/osmc/ha2
+git add .
+git status
+echo -n "Enter the Description for the Change: "
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin new
+
+scp -p 22 ps4-wake.credentials.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 ps4-games.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 secrets.yaml  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+
+exit
