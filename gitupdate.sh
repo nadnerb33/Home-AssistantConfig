74032345fa62589dc2115301627ee128133b0801
nadnerb33 <nadnerb@gmail.com>
2020-01-19 20:29:26 +0000

    change2

--- gitupdate.sh
+++ gitupdate.sh
@@ -9,8 +9,8 @@ git commit -m "${CHANGE_MSG}"
 git push origin new
 
 #scp -p 22 .ps4-wake.credentials.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
-scp -p 22 .ps4-games.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
-scp -p 22 secrets.yaml  root@192.168.0.10:/i-data/md0/admin/backup/ha/
-scp -p 22 pythpn_scripts/wastecollection.py root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 /home/osmc/ha2/.ps4-games.json  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 /home/osmc/ha2/secrets.yaml  root@192.168.0.10:/i-data/md0/admin/backup/ha/
+scp -p 22 /home/osmc/ha2/pythpn_scripts/wastecollection.py root@192.168.0.10:/i-data/md0/admin/backup/ha/
 
 exit
